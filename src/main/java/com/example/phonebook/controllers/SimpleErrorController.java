f4cdfed40383d576fb4accb5f801ef317e1d684c
Vladimir Ignatev <vladimir_ignatev@epam.com>
2020-09-11 14:16:54 +0300

    Update SimpleErrorController

--- src/main/java/com/example/phonebook/controllers/SimpleErrorController.java
+++ src/main/java/com/example/phonebook/controllers/SimpleErrorController.java
@@ -3,6 +3,7 @@ package com.example.phonebook.controllers;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,9 +12,11 @@ public class SimpleErrorController implements ErrorController{
     private static final String PATH = "/error";
 
     @RequestMapping(value = "/error")
-    public String openError(Model model) {
-        model.addAttribute("message", "Something went wrong!");
-        model.addAttribute("error", "Unknown error");
+    public String showError(@RequestAttribute(value = "errorMessage", required = false) String errorMessage,
+                            @RequestAttribute(value = "errorDetails", required = false) String errorDetails,
+                            Model model) {
+        model.addAttribute("errorMessage", errorMessage);
+        model.addAttribute("errorDetails", errorDetails);
         return "error";
     }
 
